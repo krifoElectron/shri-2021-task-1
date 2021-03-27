2eedd85a95c33cafcfb255cb654cfb94c274dd13
Gordeev Arsenii <Gordeev.A.Vladim@sberbank.ru>
2021-03-27 13:46:46 +0300

    nodemon and indes.html added

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express');
+import express from 'express';
+import path from 'path';
+
+// const __dirname = path.resolve(); // process.cwd();
+const PORT = 8080;
+
 const app = express();
-const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('First commit');
+  // res.send('First commit 7!');
+  res.sendFile(path.resolve('static', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}`);
 });
