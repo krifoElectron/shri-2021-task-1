a62c48d093bd0c113371b67e56d2354ff7d47c5e
Gordeev Arsenii <Gordeev.A.Vladim@sberbank.ru>
2021-03-29 23:59:55 +0300

    refactoring the done part

---
+++ src/pages/leaders/leaders.js
@@ -0,0 +1,73 @@
+import { createElementWithCn } from '../../core';
+import './leaders.scss';
+
+const MAP_NUMBER_TO_STRING = {
+  1: 'first',
+  2: 'second',
+  3: 'third',
+};
+
+export const getLeadersElement = (users) => {
+  const leadersElement = createElementWithCn({
+    tagName: 'div',
+    className: `leaders-page`,
+  });
+
+  const thirdPlaceOnThePodium = createPlaceOnThePodium(3, users[2]);
+  const firstPlaceOnThePodium = createPlaceOnThePodium(1, users[0]);
+  const secondPlaceOnThePodium = createPlaceOnThePodium(2, users[1]);
+
+  leadersElement.appendChild(thirdPlaceOnThePodium);
+  leadersElement.appendChild(firstPlaceOnThePodium);
+  leadersElement.appendChild(secondPlaceOnThePodium);
+
+  return leadersElement;
+};
+
+function createPlaceOnThePodium(placeNumber, user) {
+  const placeOnThePodium = createElementWithCn({
+    tagName: 'div',
+    className:
+      'leaders-page__place-on-podium' +
+      ` leaders-page__place-on-podium_place_${MAP_NUMBER_TO_STRING[placeNumber]}`,
+  });
+
+  const placePersonBlock = createElementWithCn({
+    tagName: 'div',
+    className: 'leaders-page__person-block',
+  });
+
+  const imgElement = createElementWithCn({
+    tagName: 'img',
+    className: 'leaders-page__leaders-img',
+  });
+  imgElement.src = `images/3x/${user.avatar}`;
+
+  const placeName = createElementWithCn({
+    tagName: 'div',
+    textContent: user.name,
+    className: 'leaders-page__persons-name',
+  });
+
+  const placeCommits = createElementWithCn({
+    tagName: 'div',
+    textContent: user.valueText,
+    className: 'leaders-page__persons-commits',
+  });
+
+  const placePedestal = createElementWithCn({
+    tagName: 'div',
+    textContent: placeNumber,
+    className:
+      'leaders-page__pedestal' +
+      ` leaders-page__pedestal_place_${MAP_NUMBER_TO_STRING[placeNumber]}`,
+  });
+
+  placePersonBlock.appendChild(imgElement);
+  placePersonBlock.appendChild(placeName);
+  placePersonBlock.appendChild(placeCommits);
+  placeOnThePodium.appendChild(placePersonBlock);
+  placeOnThePodium.appendChild(placePedestal);
+
+  return placeOnThePodium;
+}
