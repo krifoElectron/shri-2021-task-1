8a86d5204fa29ee3f02a88d220e0f25b8894dc0f
Gordeev Arsenii <Gordeev.A.Vladim@sberbank.ru>
2021-04-01 23:02:52 +0300

    try teme dark

--- src/pages/leaders/leaders.js
+++ src/pages/leaders/leaders.js
@@ -1,5 +1,5 @@
 import { createElementWithCn } from '../../core';
-import { bemCn } from '../../utils/bem';
+import { bemCn, classNames } from '../../utils/bem';
 import './leaders.scss';
 
 const MAP_NUMBER_TO_STRING = {
@@ -8,12 +8,18 @@ const MAP_NUMBER_TO_STRING = {
   3: 'third',
 };
 
+const bindClassNames = (bindClassName) => (...cns) => {
+  return classNames(bindClassName, ...cns);
+};
+
+const classNamesWithDark = bindClassNames('theme_dark');
+
 const cn = bemCn('leaders-page');
 
 export const getLeadersElement = (users) => {
   const leadersElement = createElementWithCn({
     tagName: 'div',
-    className: cn(),
+    className: classNamesWithDark(cn()),
   });
 
   const thirdPlaceOnThePodium = createPlaceOnThePodium(3, users[2]);
@@ -30,42 +36,44 @@ export const getLeadersElement = (users) => {
 function createPlaceOnThePodium(placeNumber, user) {
   const placeOnThePodium = createElementWithCn({
     tagName: 'div',
-    className:
-      'leaders-page__place-on-podium' +
-      ` leaders-page__place-on-podium_place_${MAP_NUMBER_TO_STRING[placeNumber]}`,
+    className: classNamesWithDark(
+      cn('place-on-podium', { place: MAP_NUMBER_TO_STRING[placeNumber] }),
+      'dark_theme'
+    ),
+    // 'leaders-page__place-on-podium' +
+    // ` leaders-page__place-on-podium_place_${MAP_NUMBER_TO_STRING[placeNumber]}`,
   });
 
   const placePersonBlock = createElementWithCn({
     tagName: 'div',
-    className: `leaders-page__person-block${
-      placeNumber === 1 ? ' leaders-page__person-block_first' : ''
-    }`,
+    className: classNamesWithDark('person-block', { first: placeNumber === 1 }),
+    // `leaders-page__person-block${
+    // placeNumber === 1 ? ' leaders-page__person-block_first' : ''
+    // }`,
   });
 
   const imgElement = createElementWithCn({
     tagName: 'img',
-    className: 'leaders-page__leaders-img',
+    className: classNamesWithDark(cn('leaders-img')),
   });
   imgElement.src = `images/3x/${user.avatar}`;
 
   const placeName = createElementWithCn({
     tagName: 'div',
     textContent: user.name,
-    className: 'leaders-page__persons-name',
+    className: classNamesWithDark(cn('persons-name')),
   });
 
   const placeCommits = createElementWithCn({
     tagName: 'div',
     textContent: user.valueText,
-    className: 'leaders-page__persons-commits',
+    className: classNamesWithDark(cn('persons-commits')),
   });
 
   const placePedestal = createElementWithCn({
     tagName: 'div',
     textContent: placeNumber,
-    className:
-      'leaders-page__pedestal' +
-      ` leaders-page__pedestal_place_${MAP_NUMBER_TO_STRING[placeNumber]}`,
+    className: classNamesWithDark(cn('pedestal', { place: MAP_NUMBER_TO_STRING[placeNumber] })),
   });
 
   placePersonBlock.appendChild(imgElement);
